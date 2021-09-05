f95698268af2982c040887958b86d40c2836b24e
jyturrieta <63125197+jyturrieta@users.noreply.github.com>
2021-09-05 17:56:21 -0300

    Create insert_update.js

---
+++ insert_update.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('clase4', 'root', '',{
+    host:'localhost',
+    dialect: 'mariadb'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+class Vehiculo extends Sequelize.Model {}
+Vehiculo.init({
+  marca: Sequelize.STRING,
+  modelo:Sequelize.STRING,
+  anio: Sequelize.INTEGER
+}, { sequelize, modelName: 'vehiculos' });
+
+
+sequelize.sync().then(() => Vehiculo.create({
+    marca: 'Renault',
+    modelo: 'Kwid',
+    anio: 2017
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  });
+
+Vehiculo.update({ 
+  marca: "Honda" }, {
+  where: {
+    anio: 2017
+  }
+}).then(() => {
+  console.log("Actualizado");
+});
+  
